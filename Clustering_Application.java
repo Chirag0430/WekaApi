72994a8238bf60cca037ddd4ef03c03e6d4add05
Chirag0430 <101520308+Chirag0430@users.noreply.github.com>
2023-04-27 10:07:13 +0530

    Update Clustering_Application.java

--- Clustering_Application.java
+++ Clustering_Application.java
@@ -1,13 +1,16 @@
-package com.main;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import javax.swing.*;
+
+import weka.clusterers.ClusterEvaluation;
 import weka.core.*;
 import weka.core.converters.*;
+import weka.core.pmml.jaxbbindings.Application;
 import weka.filters.*;
 import weka.filters.unsupervised.attribute.Remove;
 import weka.clusterers.SimpleKMeans;
+import weka.gui.explorer.ClustererPanel;
 
 public class Clustering_Application extends JFrame implements ActionListener {
     private JFileChooser fileChooser;
@@ -80,6 +83,10 @@ public class Clustering_Application extends JFrame implements ActionListener {
                     Instances data = new Instances(reader);
                     reader.close();
                     txtData.setText(data.toString());
+                } catch (FileNotFoundException ex) {
+                    JOptionPane.showMessageDialog(this, "Error: File not found.");
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(this, "Error reading file: " + ex.getMessage());
                 } catch (Exception ex) {
                     JOptionPane.showMessageDialog(this, "Error loading ARFF file: " + ex.getMessage());
                 }
@@ -93,11 +100,14 @@ public class Clustering_Application extends JFrame implements ActionListener {
                     writer.write(txtData.getText());
                     writer.close();
                     JOptionPane.showMessageDialog(this, "ARFF file saved successfully.");
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(this, "Error saving file: " + ex.getMessage());
                 } catch (Exception ex) {
                     JOptionPane.showMessageDialog(this, "Error saving ARFF file: " + ex.getMessage());
                 }
             }
-        } else if (e.getSource() == btnLoadCSV) {
+        }
+        else if (e.getSource() == btnLoadCSV) {
             // load CSV file
             if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
                 inputFile = fileChooser.getSelectedFile();
@@ -126,26 +136,50 @@ public class Clustering_Application extends JFrame implements ActionListener {
         } else if (e.getSource() == btnFilterAttributes) {
             // filter attributes
             try {
+                Instances data = getInstancesFromTextArea();
+
+                // apply Remove filter to remove attributes 1 to 3
                 Remove remove = new Remove();
-                remove.setOptions(new String[]{"-R", "1-3"});
-                remove.setInputFormat(getInstancesFromTextArea());
-                Instances filteredData = Filter.useFilter(getInstancesFromTextArea(), remove);
-                txtData.setText(filteredData.toString());
+                remove.setAttributeIndices("1-3");
+                remove.setInputFormat(data);
+                Instances filteredData = Filter.useFilter(data, remove);
+
+                // display filter output
+                txtData.setText("Filtered data:\n\n");
+                txtData.append(filteredData.toString());
+
+                // display filter summary
+                String[] options = remove.getOptions();
+                txtData.append("\n\nFilter summary:\n\n");
+                txtData.append("Filter type: " + remove.getClass().getSimpleName() + "\n");
+                txtData.append("Attribute indices to remove: " + options[1] + "\n");
+                txtData.append("Number of attributes removed: " + filteredData.numAttributes() + "\n");
+                txtData.append("Number of instances remaining: " + filteredData.numInstances() + "\n");
             } catch (Exception ex) {
                 JOptionPane.showMessageDialog(this, "Error filtering attributes: " + ex.getMessage());
             }
-        } else if (e.getSource() == btnClusterData) {
+        }
+        else if (e.getSource() == btnClusterData) {
             // cluster data
             try {
                 SimpleKMeans kMeans = new SimpleKMeans();
                 kMeans.setNumClusters(3);
                 kMeans.buildClusterer(getInstancesFromTextArea());
-                txtData.setText(kMeans.toString());
+
+                // evaluate the clusterer and display results
+                ClusterEvaluation eval = new ClusterEvaluation();
+                eval.setClusterer(kMeans);
+                eval.evaluateClusterer(getInstancesFromTextArea());
+                txtData.setText(eval.clusterResultsToString());
+
+                revalidate();
             } catch (Exception ex) {
                 JOptionPane.showMessageDialog(this, "Error clustering data: " + ex.getMessage());
             }
         }
+
     }
+
 
     private Instances getInstancesFromTextArea() throws Exception {
         BufferedReader reader = new BufferedReader(new StringReader(txtData.getText()));
@@ -158,4 +192,3 @@ public class Clustering_Application extends JFrame implements ActionListener {
         new Clustering_Application();
     }
 }
-
